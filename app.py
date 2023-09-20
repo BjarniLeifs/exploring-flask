df00b6098bd196d4e02dc75c741f8da1dd565615
Bjarni Kristján Leifsson <bjarnikr@msn.com>
2023-09-20 00:16:04 +0000

    Update app.py
    add new routes with string and int variable... looking into how that is passed.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def welcome():
     return "Hello World!"
 
+@app.route('/<int:number>/')
+def incrementer(number):
+    return "Incremented number is " + str(number + 1)
+
+@app.route('/<string:name>/')
+def hello(name):
+    return "Hello " + name
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
