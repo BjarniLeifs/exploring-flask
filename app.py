1bd1e6d66a535aa46cdec553a546cae1a08f235e
Bjarni Kristján Leifsson <bjarnikr@msn.com>
2023-09-20 12:04:20 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import jsonify
 
 app = Flask(__name__)
 
+@app.before_request
+def before():
+    print ("This is executed before each request")
+    
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
@@ -26,6 +30,20 @@ def person():
 @app.route('/numbers')
 def print_list():
     return jsonify(list(range(5)))
+
+''' looking into redirection behaviour /test/ vs /test '''
+@app.route('/home/')
+def home():
+    return "Home page"
+
+@app.route('/contact')
+def contact():
+    return "Contct page"
+
+''' return status code'''
+@app.route('/status')
+def status():
+    return "would you like to see 418", 418
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
